528df08aad66180af95b6d8209edb1a0926782a7
alvir85 <alvin85@mail.fresnostate.edu>
2021-11-15 05:12:06 -0800

    Start of query search for IDs

---
+++ scripts/query.js
@@ -0,0 +1,9 @@
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js'
+import app from './initApp.js'
+
+var db = getFirestore(app);
+
+async function query(searchID) {
+	const hNameID = db.child('hNameID');
+	const query = hNameID.orderByChild('hNameID').equalTo(searchID)
+}
