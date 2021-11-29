51f7ab8aa5e3e7c1cbdb8c4d990d823bbc225c4b
alvir85 <alvin85@mail.fresnostate.edu>
2021-11-28 23:58:08 -0800

    commit

--- scripts/household_Database.js
+++ scripts/household_Database.js
@@ -7,6 +7,13 @@ var db = getFirestore(app);
 const varDoc = collection(db, "Household_database");
 const varDoc2 = collection(db, "user_database");
 
+var billNames = [];
+var billAmounts = [];
+billAmounts.length = 6;
+billNames.length = 6;
+var b = 0;
+var c = 0;
+
 //Questionare calling the setter functions
 document.getElementById('submitButton').addEventListener('click', (e) => {
     e.preventDefault();
@@ -17,11 +24,35 @@ document.getElementById('submitButton').addEventListener('click', (e) => {
     var inNoRoom = parseInt(document.getElementById('numBedrooms').value);
     var inUserName = document.getElementById('usernameInput').value;
     //var inHouse = document.getElementById('apt').value;
+
+    //var billAvg = 100/inHsize;
+   
+    var tempCheck = [
+                    document.getElementById('rentBill'), document.getElementById('electricBill'), document.getElementById('gasBill'),
+                    document.getElementById('waterBill'), document.getElementById('garbageBill'), document.getElementById('internetBill')
+                    ];
+    var tempAmt = [
+                    document.getElementById('rentAmount').value, document.getElementById('electAmount').value, document.getElementById('gasAmount').value,
+                    document.getElementById('waterAmount').value, document.getElementById('garbAmount').value, document.getElementById('intAmount').value
+                    ];
+
+    for(let a = 0; a < 6; a++){
+        if (tempCheck[a].checked == true){
+            billNames[b] = tempCheck[a].value;
+            b++;
+        }
+        if (tempAmt[a] != ""){
+            billAmounts[c] = tempAmt[a];
+            c++;
+        }
+    }
+
     newHouse(inHname, inHsize, inApt, inNoRoom, inUserName); 
 })
 
 //creates a new document in household_database
 async function newHouse(input1,input2,input3,input4,input5){
+    var billAvg = 100/input2;
     const newHouseDoc = await addDoc(varDoc, {
         hName: input1,
         hSize: input2,
@@ -30,14 +61,12 @@ async function newHouse(input1,input2,input3,input4,input5){
         rumiis: [input5],
     })
     .then(function(docRef) {
+        makeBills(billAmounts, billAvg, billNames, docRef.id);
         // adds the houseID into the house doc itself, in case we need it
         setDoc(doc(varDoc, docRef.id), {
             houseID: docRef.id
         },
         {merge: true});
-        setDoc(collection(db, "Household_database", docRef.id), {
-            bills
-        });
         // adds the houseID into the user doc, not done yet
         // setDoc(doc(varDoc2, getAuth()), {
         //     houseID: docRef.id
@@ -48,6 +77,18 @@ async function newHouse(input1,input2,input3,input4,input5){
         console.error("error: ", error);
     });
 
+}
+
+async function makeBills(bamt, bavg, bname, hid){
+
+    for(let i = 0; i < bname.length; i++ ){
+         await setDoc(doc(db, "Household_database", "08RQSrjL9vjIhH14yXMp", "Bills", bname[i]), {
+            amount : parseInt(bamt[i]),
+            billPer : bavg
+            // due : dueDateArr[i]
+         });
+        }
+         //insert for loop when custom billPer  is implemented
 }
 
 /////////////////////////////////////////////////////////////////////////////
