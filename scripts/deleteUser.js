021375d0f724e5e3e06312c21f153e389822e6fc
alvir85 <alvin85@mail.fresnostate.edu>
2021-12-01 20:08:07 -0800

    deleted commented funcion

--- scripts/deleteUser.js
+++ scripts/deleteUser.js
@@ -6,7 +6,6 @@ var db = getFirestore(app);
 
 const auth = getAuth();
 document.getElementById('delButton').addEventListener('click', (e) => {
-    e.preventDefault();
     const userID = auth.currentUser.uid;
     deleteUser(userID)
 });
@@ -34,27 +33,3 @@ async function deleteUser(userIDinput){
         }
     }
 }
-
-// async function deleteUser(inputHID){
-//     const user = getAuth().currentUser;
-//     const houseSnap = await getDoc(doc(db,"Household_database",inputHID));
-//     var rumiiArr = houseSnap.data().rumiis
-//     var printToConsole = "error didnt go through the loop"
-//     for (let i = 0; i < rumiiArr.length; i++){
-//         if((currentDoc.data().userName) == rumiiArr[i]){
-//             rumiiArr.splice(i, 1);
-//             printToConsole = "sucessfully deleted"
-//         } else {
-//         }
-//     }
-//     setDoc(doc(db, "Household_database", inputHID), {
-//         rumiis: rumiiArr
-//     },
-//     {merge: true});
-            
-//     setDoc(doc(db, "User_database", user), {
-//         houseID: deleteField()
-//     },
-//     {merge: true});
-//     return printToConsole;
-// }
