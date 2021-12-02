c332ee92bdcbccf72b3cf1580d270fba322f2882
MSeechan <66396292+MSeechan@users.noreply.github.com>
2021-12-01 18:52:26 -0800

    display bill and amount as lists on homepage

--- scripts/billPreview.js
+++ scripts/billPreview.js
@@ -8,27 +8,35 @@ const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
     const uid = user.uid;
-    get_billDue(db,uid);
-    
-} else {
-  // User not signed in, redirect back to signin page
-  console.log("Please Sign in")
-  window.location = 'index.html'; 
-}
+    get_billDue(db, uid);
+
+  } else {
+    // User not signed in, redirect back to signin page
+    console.log("Please Sign in")
+    window.location = 'index.html';
+  }
 });
 
-async function get_billDue(db,uid){
-    const docRef = doc(db, "User_database", uid);
-    const docSnap = await getDoc(docRef);
-    var hID = docSnap.data().houseID;
+async function get_billDue(db, uid) {
+  const docRef = doc(db, "User_database", uid);
+  const docSnap = await getDoc(docRef);
+  var hID = docSnap.data().houseID;
 
-   const querySnapshot = await getDocs(collection(db, 'Household_database',hID, "Bills"));
-   querySnapshot.forEach((doc) => {
-        var node = document.createElement("LI");                 
-        var bill = document.createTextNode(`${doc.id} :\t ${doc.data().amount}`);         
-        node.appendChild(bill);                          
-        document.getElementById("billPreview").appendChild(node);    
-   })
+  // List for bill names
+  const querySnapshot = await getDocs(collection(db, 'Household_database', hID, "Bills"));
+  querySnapshot.forEach((doc) => {
+    var node = document.createElement("LI");
+    var bill = document.createTextNode(` ${doc.id}`);
+    node.appendChild(bill);
+    document.getElementById("billsID").appendChild(node);
+  })
+  // List for bill amounts
+  querySnapshot.forEach((doc) => {
+    var node = document.createElement("LI");
+    var bill = document.createTextNode(`${doc.data().amount}`);
+    node.appendChild(bill);
+    document.getElementById("billAmountsID").appendChild(node);
+  })
 };
 
 
